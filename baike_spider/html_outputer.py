3039680200e26e5e8c94e0c7486f082593a690c9
wjlucc <wjlucc@gmail.com>
2015-12-27 16:52:43 +0800

    html_outputer complete

--- baike_spider/html_outputer.py
+++ baike_spider/html_outputer.py
@@ -5,19 +5,35 @@ Created on 2015年12月27日
 @author: WJLUCK
 '''
 
-
-
-
 class HtmlOutputer(object):
     
+    def __init__(self):
+        self.datas = []  #list用来存放收集到的全部数据，每个元素是个dict结构。
     
-    def collect_data(self):
-        pass
+    def collect_data(self, data): #用于收集数据
+        if data is None:
+            return 
+        self.datas.append(data)
 
     
-    def output_html(self):
-        pass
-    
+    def output_html(self):  #用于将收集好的数据写出到一个html文件中。对象自身维护一个list表。
+        fout = open('result.html','w')
+        fout.write('<html>')
+        fout.write('<body>')
+        fout.write('<table>')
+        for data in self.datas:
+            fout.write('<tr>')
+            fout.write('<td>%s</td>' % data['title'].encode('utf-8'))
+            fout.write('<td>%s</td>' % data['summary'].encode('utf-8'))
+            fout.write('</tr>')        
+        fout.write('</table>')
+        fout.write('</body>')
+        fout.write('</html>')
+        fout.close()
+        
+        
+
+
     
     
     
