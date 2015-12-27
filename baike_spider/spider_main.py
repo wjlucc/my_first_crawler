b79e5dc8ba396d2d0a0a3f80c0edb25af1a9bf1f
wjlucc <wjlucc@gmail.com>
2015-12-27 13:40:31 +0800

    delete

--- baike_spider/spider_main.py
+++ baike_spider/spider_main.py
@@ -6,9 +6,11 @@ Created on 2015年12月27日
 '''
 
 #为什么要把这个爬虫类创建在这里？创建在一个新的模块中会更好？
-#这个爬虫总调度程序会使用url管理器，html下载器何解析器
+#这个爬虫总调度程序会使用url管理器，html下载器和解析器，输出器来完成所需要的功能。
+#用到的这些模块会在该爬虫的构造函数中初始化。
 class SpiderMain(object):
-    
+    def __init__(self):
+        self.urls = urls
     
     def craw(self, root_url):
         pass
